c2c34656aed2544374d97308c3cec25e71ef4283
Fagundes1993 <mfbl93@gmail.com>
2023-08-26 15:08:31 -0300

    Adição da aula sobre operadores lógicos AND e OR

---
+++ 02 - Explorando Queries com SQL/05_and_or.sql
@@ -0,0 +1,7 @@
+-- Operadores Lógicos
+
+select * from employee;
+
+select Bdate, Address from employee where Fname = 'William' and Lname = 'Cardinale';
+
+select Bdate, Address from employee where Fname = 'William' or Lname = 'Wellington';
